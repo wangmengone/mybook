34466d674a90d11487ff72f9e6bb6aa07704a232
wang <458653638@qq.com>
2018-08-14 16:01:02 +0800

    修改函数值

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
 
-var app = express();
+var app = express(234);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
